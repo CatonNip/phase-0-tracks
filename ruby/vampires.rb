548fde8665206a735afefee975f69f787fa26234
CatonNip <vivinguyen000@gmail.com>
2016-08-03 18:08:18 -0700

    Add questions to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name? *looks at you suspiciously* ( ͡ಠ ʖ̯ ͡ಠ)"
+name = gets.chomp 
+puts "☜(`o´) How old are you?!"
+age = gets.chomp
+puts "What year were you born?"
+year = gets.chomp
+puts "(ಠ ∩ಠ)Our company cafeteria serves garlic bread."
+puts "Should we order some for you?(yes/no)"
+garlic_bread = gets.chomp
+puts "Would you like to enroll in the company’s health insurance? HUH?! Do ya punk?! ೕ(Ò⺫ Ó )೨"
