d73a82d3742e05651e1cfa2a06e560af739d3809
CatonNip <vivinguyen000@gmail.com>
2016-08-08 12:53:26 -0700

    Add r-2 use arrays in method

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,12 @@
+def build_array(input_1,input_2,input_3)
+	new_array = []
+	new_array << input_1 << input_2 << input_3
+end
+
+def add_to_array(array,input)
+	array << input
+end
+
 food = []
 p food 
 
@@ -19,3 +28,8 @@ p food.include?("mangos")
 drinks = ["oj","beer","tea"]
 menu = food + drinks
 p menu
+
+p build_array(1,2,3)
+p build_array(nil,true,"cats")
+p add_to_array(menu,"coffee")
+p add_to_array(menu,"ice cream")
