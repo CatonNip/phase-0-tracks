a324d7f2c80f209c21f1a44b3dc2e5e45befaf38
CatonNip <vivinguyen000@gmail.com>
2016-08-13 07:31:43 -0700

    Add new values for methods in interation.rb

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,6 +6,7 @@ def greeting
 	yield(name1, name2)
 end
 greeting {|name1,name2| puts "Nice seeing you today #{name1} and #{name2}."}
+
 ########release 1#######
 #declare a new array
 new_food = ["pasta","corn","apple","oranges","mangos"]
@@ -25,6 +26,7 @@ differnt_animals.each do |type,animals|
 	p "I really like #{type}s like the #{animals}."
 end
 p differnt_animals
+
 ####release 2########
 #A method that iterates through the items, deleting any that meet a certain condition (for example, deleting any numbers that are less than 5).
 #make an array 
@@ -72,7 +74,7 @@ test_hash = {
 }
 
 def numbers_condition(test_list)
-	test_list.drop_while {|list| list <= 1}
+	test_list.drop_while {|list| list <= 2}
 end
 
 def movie_type(test_hash)
