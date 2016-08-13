020db34a3cf08594ad0c46aebcbcf9aff0dfa744
CatonNip <vivinguyen000@gmail.com>
2016-08-13 11:15:52 -0700

    Add identified methods and apply them to strings to modify them.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,48 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+#"string".swapcase returns an copy of the string with the character case converted. 
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+#"string".insert() other_str before the charater at the given index, modifying str.
+puts "zom".insert(1,'o')
 # => “zoom”
 
-# "enhance".<???>
+#"string".center() centers a string width 
+puts "enhance".center(20)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+#"string".upcase replaces all lowercase alphabet charaters with upercase.
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+puts "the usual".insert(-1," suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+puts " suspects".insert(0,"the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+#"string".chomp() returns a new string with the given selector removed from the end of the string.
+puts "The case of the disappearing last letter".chomp('r')
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".chomp!
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+#"sting".squeeze()Returns a new string where runs of the same character that 
+# occur in this set are replaced by a single character. If no arguments are given, 
+# all runs of identical characters are replaced by a single character.
+puts "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts "z".bytes
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
+
+
+
+
