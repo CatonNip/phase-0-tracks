5aec01575c4a2e69d0b8fee70544cc40fc81cf6d
CatonNip <vivinguyen000@gmail.com>
2016-08-14 00:45:30 -0700

    Write pseudocode for alias_manager.rb

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,8 @@
+####Pseudocode####
+#prompt the agent to input their name
+#save that name into a data stucture 
+#create small methods so you can link them together
+#translate if a letter within the data structure is a vowel or consonant
+#look up modual to transfer charaters and/or indexes of a data structure
+#make all of the inputs .downcase or .upcase to filter though inputs that might have both
+#######
