01859c594602908dbb1749bc67bff01c4b16fad6
CatonNip <vivinguyen000@gmail.com>
2016-08-15 00:00:31 -0700

    Add methods to alias_manage.rb

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,5 +1,4 @@
 ####Pseudocode####
-#Release 0 
 #prompt the agent to input their name
 #save that name into a data stucture 
 #create small methods so you can link them together
@@ -12,6 +11,35 @@
 	#tell me if an item on a data structure is a vowel
 	#tell me if an item on a data stucture is a consonant
 	#convert the user's original name into an alias 
-
-#Vivi, start with this first and worry about the other stuff later. You got this!
 #######
+
+def agent_real_name
+	puts "Hello there Agent. Please enter your first name. If you daaare!!!"
+		firstname = gets.chomp.downcase
+# => here is where my code kept breaking until I compined the stings into a variable
+	puts "Now enter your last name."
+		lastname = gets.chomp.downcase
+	name = lastname + " " + firstname
+end
+
+def agents_alias(agent_real_name)
+	fake_name = agent_real_name.split('')
+	fake_name.length.times do |edit|
+		fake_name[edit] = vowels_consonants(fake_name[edit])
+	end
+	fake_name.join('')
+end
+
+def vowels_consonants(letter)
+	vowels = "aeiou"
+	consonants = "bcdfghjklmnpqrstvwxyz"
+	if vowels.include?(letter)
+		vowels[vowels.index(letter) + 1]
+	elsif consonants.include?(letter)
+ 		consonants[consonants.index(letter) + 1]
+	else
+		letter
+	end
+end
+
+puts agents_alias(agent_real_name).capitalize	
