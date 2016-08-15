e8f0277267f3148e500f389b430199b96c2da4e5
CatonNip <vivinguyen000@gmail.com>
2016-08-15 12:16:59 -0700

    Design class for puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+# SPECIES ------------------------
+# puppy
+
+# CHARACTERISTICS ----------------
+# Body Type: varies
+# Number of paws: 4
+# Friendly: yes
+# Adorable: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Play Dead
+# Lick
+# Play
+# Howl
+# Sleep
+# Jump
+# Hug
+# Paw
