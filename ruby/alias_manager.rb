e54d68417e0301f3810c0d180533cf38d9f6b612
CatonNip <vivinguyen000@gmail.com>
2016-08-14 21:11:57 -0700

    Update psedocode for release 0 in alias_manager.rb

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,8 +1,17 @@
 ####Pseudocode####
+#Release 0 
 #prompt the agent to input their name
 #save that name into a data stucture 
 #create small methods so you can link them together
 #translate if a letter within the data structure is a vowel or consonant
 #look up modual to transfer charaters and/or indexes of a data structure
 #make all of the inputs .downcase or .upcase to filter though inputs that might have both
+
+#I need to def methods that:
+	#gathers the user's name and convert it to a data structure
+	#tell me if an item on a data structure is a vowel
+	#tell me if an item on a data stucture is a consonant
+	#convert the user's original name into an alias 
+
+#Vivi, start with this first and worry about the other stuff later. You got this!
 #######
