ba8f10b4ca05232fd8d8bf8993f469b53a11b81c
CatonNip <vivinguyen000@gmail.com>
2016-08-16 16:04:59 -0700

    Pseudocode pokemon class

---
+++ ruby/solo.rb
@@ -0,0 +1,11 @@
+# Pokemon ------------------------
+
+# CHARACTERISTICS ----------------
+# Type: varies (fire,water,grass,ghost,psychic,bug,dragon,ice,
+# fighting,flying,ground,electic,normal,poison,rock)
+# weakness: varies
+# Name: varies
+
+# BEHAVIOR -----------------------
+# attack
+# being adorable 
