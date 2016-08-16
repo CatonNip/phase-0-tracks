fbb3a891f52543ab44494840e5c83a70e7d9f3c9
CatonNip <vivinguyen000@gmail.com>
2016-08-16 15:18:54 -0700

    Driver code add to santa.rb

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,8 @@
 class Santa
-
 	def initialize(gender,ethnicity)
 		puts "initializing Santa instance..."
-		@gender = "non-binary"
-		@ethnicity = "German"
+		@gender = gender
+		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
 	end
@@ -18,12 +17,29 @@ class Santa
 		puts "That was a good #{cookie}!"
 		cookie
 	end
-
 end
 
-#UI
+santas = []
+example_genders = ["non-binary","agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
-# Frank = Santa.new
+until santas.length == 50
+  santas << Santa.new(example_genders.sample, example_ethnicities.sample)
+end
+
+p santas
+
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+# Frank = Santa.new("non-binary","French")
 # cookie = "chocolate chip"
 # Frank.speak
 # Frank.eat_milk_and_cookies(cookie)
+# Frank.about
+
