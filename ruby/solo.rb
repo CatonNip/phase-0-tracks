561c5301de12217e930d9a088b35c7205a1707af
CatonNip <vivinguyen000@gmail.com>
2016-08-16 16:11:22 -0700

    Add class Pokemon and initialize method

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,11 +1,25 @@
 # Pokemon ------------------------
-
+#first gen 
 # CHARACTERISTICS ----------------
 # Type: varies (fire,water,grass,ghost,psychic,bug,dragon,ice,
 # fighting,flying,ground,electic,normal,poison,rock)
+# => pokemon type source (http://pokemon.wikia.com/wiki/Pok%C3%A9mon_types)
 # weakness: varies
 # Name: varies
 
 # BEHAVIOR -----------------------
 # attack
 # being adorable 
+
+class Pokemon 
+
+	def initialize(name)
+		puts "Initializing Pokemon class...beep boop beep boop"
+		@name = name 
+		@type = ["fire","water","grass","ghost","psychic","bug","dragon","ice",
+				"fighting","flying","ground","electic","normal","poison","rock"]
+		@weakness = ["fire","water","grass","ghost","psychic","bug","dragon","ice",
+				"fighting","flying","ground","electic","normal","poison","rock"]
+	end
+
+end
