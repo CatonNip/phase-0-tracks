a83571feaf0683da868fbdbbf48384f517bd3c92
CatonNip <vivinguyen000@gmail.com>
2016-08-16 15:40:03 -0700

    Add setter and getter methods and two new methods to santa.rb

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,15 +7,36 @@ class Santa
 		@age = 0
 	end
 
-
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
-
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 		cookie
+	end
+
+	def celebrate_brithday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking << reindeer
+		p @reindeer_ranking
+	end
+	#getter methods
+	def age
+		@age +=
+		puts "Santa you're #{age} years old now!"
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+	#setter method
+	def gender=(new_gender)
+		@gender = new_gender
 	end
 end
 
@@ -23,11 +44,12 @@ santas = []
 example_genders = ["non-binary","agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
-until santas.length == 50
+until santas.length == 3
   santas << Santa.new(example_genders.sample, example_ethnicities.sample)
 end
-
 p santas
+
+
 
 # santas << Santa.new("agender", "black")
 # santas << Santa.new("female", "Latino")
@@ -37,9 +59,9 @@ p santas
 # santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
 # santas << Santa.new("N/A", "N/A")
 
-# Frank = Santa.new("non-binary","French")
-# cookie = "chocolate chip"
-# Frank.speak
-# Frank.eat_milk_and_cookies(cookie)
-# Frank.about
-
+Frank = Santa.new("non-binary","French")
+cookie = "chocolate chip"
+Frank.speak
+Frank.eat_milk_and_cookies(cookie)
+Frank.get_mad_at("Dasher")
+Frank.celebrate_brithday
