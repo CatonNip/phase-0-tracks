b50c00a6bfc1acc7f5028a484a9f73feb151058e
CatonNip <vivinguyen000@gmail.com>
2016-08-17 10:37:06 -0700

    Add pokemon creator method and loop

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -14,18 +14,20 @@
 #declare a pokemon class
 class Pokemon
 	#attr_reader acts as a getter method
-	attr_reader :weakness
+	attr_reader :weakness, :attack
 	#attr_accessor acts as a setter method
-	attr_accessor :name,:type,:attack
+	attr_accessor :name,:type
 	#declar initialize method and call attributes you need
-	def initialize(name)
+	def initialize(name,type)
 		#put a print statement so you know the method is working
 		puts "Who's...that...POKEMON?!"
 		#assign attributes
 		@name = name 
 
-		@type = ["fire","water","grass","ghost","psychic","bug","dragon","ice","fighting",
-				"flying","ground","electic","normal","poison","rock"]
+		@type = type
+
+				#["fire","water","grass","ghost","psychic","bug","dragon","ice","fighting",
+				#{}"flying","ground","electic","normal","poison","rock"]
 
 		@weakness = ["fire","water","grass","ghost","psychic","bug","dragon","ice","fighting",
 					"flying","ground","electic","normal","poison","rock"]
@@ -42,34 +44,52 @@ class Pokemon
 					"THUNDERBOLT and that is how mankind discovered electricity.",
 					"SELF DESTRUCT and forgets to save and commit their work to github.",
 					"TOXIC and plays League of Legends.",
-					"ROCK THROW and throws a pebble at their opponent."
-				]
+					"ROCK THROW and throws a pebble at their opponent."]
 	end
 
 	def pokemon_type
 		#type.sample returns a random element within a list.
-		puts "#{name}'s type is " + type.sample + "."
+		puts "#{@name}'s type is #{@type}."
 	end
 
 	def pokemon_weakness
-		puts "#{name}'s weakness is " + weakness.sample + "."
+		puts "#{@name}'s weakness is " + weakness.sample + "."
 	end
 
 	def pokemon_attack
-		puts "#{name} uses " + attack.sample + "."
+		puts "#{@name} uses " + attack.sample 
 	end
 
 end
 
 #UI
-# pokemon_array =
+def pokemon_creator(pokemon_array)
+	puts "What would you like to name your pokemon?"
+		name = gets.chomp
+	puts "What type of pokemon do you have?(fire,water,grass,ect.)"
+		type = gets.chomp.downcase
+	pokemon_array << Pokemon.new(name,type)
+end
+#geet the user
+puts "Welcome to the pokemon generator 2000!"
+#create empty array so you can store pokemon info
+pokemon_array = []
+pokemon_creator(pokemon_array)
 
-# puts "Welcome to the pokemon generator 2000!"
+puts "Would you like to make a new pokemon?(yes/no)"
+	new_pokemon = gets.chomp.downcase
+	new_pokemon = new_pokemon == "yes"
 
+while new_pokemon == true
+	pokemon_creator(pokemon_array)
+	puts "Would you like to make a new pokemon?(yes/no)"
+	new_pokemon = gets.chomp.downcase
+	break if new_pokemon == "no"	
+end
+p pokemon_array
 
-
-
-Haunter = Pokemon.new("Patrick")
-Haunter.pokemon_type
-Haunter.pokemon_attack
-Haunter.pokemon_weakness
+#TEST CODE
+# Haunter = Pokemon.new("Vivi")
+# Haunter.pokemon_type
+# Haunter.pokemon_attack
+# Haunter.pokemon_weakness
