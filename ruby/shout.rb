cab5d26b9188ea386803401eb7f15833dc0131ba
CatonNip <vivinguyen000@gmail.com>
2016-08-17 13:57:29 -0700

    Comment out old code in shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,13 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yelling_happily(words)
-  	words + "<3" + " ;D"
-  end
-end
+#   def self.yelling_happily(words)
+#   	words + "<3" + " ;D"
+#   end
+# end
 
-#DRIVER CODE
-p Shout.yell_angrily("DARN!")
-p Shout.yelling_happily("YAY!")
+# #DRIVER CODE
+# p Shout.yell_angrily("DARN!")
+# p Shout.yelling_happily("YAY!")
