02d3e925364366958a42602c47a0b6fe8871e6af
CatonNip <vivinguyen000@gmail.com>
2016-08-18 23:05:46 -0700

    Add reflect

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -8,6 +8,30 @@
   # print the list to the console [can you use one of your other methods here?]
 # output: HASH
 
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: 
+  # Add new item and quantity to hash
+# output: HASH
+
+# Method to remove an item from the list
+# input: Item to remove and quantity
+# steps: Delete key and value
+# output: Updated hash with removed item / quantity
+
+# Method to update the quantity of an item
+# input: item name and quantity
+# steps: 
+  #Find key of item
+  #Update item value
+# output: Hash with updated quantity
+
+# Method to print a list and make it look pretty
+# input: Grocery Hash
+# steps: 
+  # Iterate through the hash and print EACH key, value pair in a sentence
+# output: Printed grocery list
+
 def create_list(string)
 	grocery_hash = {}
 	the_list = string.split
@@ -17,21 +41,10 @@ def create_list(string)
 	print_list(grocery_hash)
 end
 
-# Method to add an item to a list
-# input: item name and optional quantity
-# steps: 
-  # Add new item and quantity to hash
-# output: HASH
-
 def add_item(item,quantity,new_list)
 	new_list[item] = quantity
 	p new_list
 end
-
-# Method to remove an item from the list
-# input: Item to remove and quantity
-# steps: Delete key and value
-# output: Updated hash with removed item / quantity
 
 def remove_item(item,new_list)
 	if new_list.has_key?(item) == true
@@ -39,27 +52,14 @@ def remove_item(item,new_list)
 	else
 		puts "ERROR."
 	end
-
 	p new_list
 end
 
-# Method to update the quantity of an item
-# input: item name and quantity
-# steps: 
-  #Find key of item
-  #Update item value
-# output: Hash with updated quantity
-
-def update_quantity(item,quantity,new_list)
-	new_list[item] = quantity
-	p new_list
-end
-
-# Method to print a list and make it look pretty
-# input: Grocery Hash
-# steps: 
-  # Iterate through the hash and print EACH key, value pair in a sentence
-# output: Printed grocery list
+# we can delete this method
+# def update_quantity(item,quantity,new_list)
+# 	new_list[item] = quantity
+# 	p new_list
+# end
 
 def print_list(new_list)
 	new_list.each do |key,value|
@@ -67,17 +67,49 @@ def print_list(new_list)
 	end
 end
 
-new_list = create_list("carrots apples cereal pizza")
-p add_item("orange",2,new_list)
-remove_item("carrots",new_list)
-update_quantity("potatoes",100,new_list)
+
+##Driver Code##
+new_list = create_list("")
+add_item("Lemonade",2,new_list)
+add_item("Tomatoes",3,new_list)
+add_item("Onions",1,new_list)
+add_item("Ice Cream",2,new_list)
+remove_item("Lemonade",new_list)
+update_quantity("Ice Cream",4,new_list)
+
 print_list(new_list)
 
+# p add_item("orange",2,new_list)
+# remove_item("carrots",new_list)
+# update_quantity("potatoes",100,new_list)
+# print_list(new_list)
 
 
+# What did you learn about pseudocode from working on this challenge?
+# => I learned that when I pseudocode I tend to really over think. 
+# => I've had a horrible habbit of just rushing into things and not taking the
+# => time to psudocode propperly. Taking just a little bit of time to wite your
+# => code in plain english makes it so much easier to understand and write the initial 
+# => solution.
 
+# What are the tradeoffs of using arrays and hashes for this challenge?
+# => When picking what data structure you use it's important to look at the code 
+# => you're writing for. The reason why my partner and I choose a hash was so we can 
+# => assign a quanity value for an item. If the progam wanted us to find an item in
+# => a grocery list and array would have been more useful and a hash.
 
+# What does a method return?
+# => A method returns the output. This output can be used as an argument for other
+#methods in your program or printed.
 
+# What kind of things can you pass into methods as arguments?
+# => You can pass other methods, data stuctures, variables, user input, ect.
 
+# How can you pass information between methods?
+# => To pass information between methods you need to set a variable to be an output of
+# => a method.
 
-
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+# => There where so many concepts that where cleaned up thanks to my amazing guide! Thanks
+# => Davina you're amazing! Psudocode-ing was one of the big ones. There are so many built
+# => in ruby methods and I'm still trying to remember them. I can't be scared to look things up.
