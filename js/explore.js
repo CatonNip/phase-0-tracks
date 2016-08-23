a933a14dc1f35acee83b4deaadfcaaf87d1cf6fc
CatonNip <vivinguyen000@gmail.com>
2016-08-23 13:43:02 -0700

    Write pseudocode to create a function that reverses a string

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// Pseudocode //
+// Input: STRING
+// Declare a FUNCTION(method) that takes a STRING as a parameter.
+	// Have a VARIABLE save data of the STRING but start as an empty string
+	// FOR each CHARACTER in the VARIABLE LOOP through them
+		// CONCATENATE each CHARACTER on the VARIABLE
+	// RETURN the changed VARIABLE
+// Output: STRING with characters reversed 
