92e235b426c68369fa59177f340f4ee649aea0d8
CatonNip <vivinguyen000@gmail.com>
2016-08-23 14:16:34 -0700

    Add reverse function and test to see if it works

--- js/explore.js
+++ js/explore.js
@@ -1,4 +1,4 @@
-// Pseudocode //
+// Pseudo code //
 // Input: STRING
 // Declare a FUNCTION(method) that takes a STRING as a parameter.
 	// Have a VARIABLE save data of the STRING but start as an empty string
@@ -8,11 +8,21 @@
 // Output: STRING with characters reversed 
 
 function reverse(string) {
-	var reversed_string = "";
-
+	var reversed_string = '';
+	for (var i = string.length - 1; i >= 0; i--) {
+		reversed_string += string[i];
+	}
+	return reversed_string;
 }
 
 
 
 
 // Driver Code //
+console.log(reverse("Meow"))
+
+
+
+// Test Code //
+// var code = "String!"
+// console.log(code)
