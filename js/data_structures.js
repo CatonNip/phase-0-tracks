b9430018becf9bede332cd066cbcb8834a9460ca
CatonNip <vivinguyen000@gmail.com>
2016-08-23 18:18:30 -0700

    Add two lists and push new items to arrays

---
+++ js/data_structures.js
@@ -0,0 +1,17 @@
+// Declare two separate lists. One for color and one for names.
+// Set variable to a list of colors
+var horseColors = ["Radiant Orchid", "Titanium White", "Cerulean", "Alizarin Crimson"];
+// Set variable to a list of names
+var horseNames = ["War Admiral", "American Pharoah","Sir Barton", "Gallant Fox"];
+// Add code to add new colors and names to the horse_names and horse_colors list
+horseColors.push("Yellow Ochre")
+horseNames.push("Bob Ross")
+
+
+
+
+
+// Driver Code //
+// To print lists
+console.log(horseColors)
+console.log(horseNames)
