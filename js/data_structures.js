506bee0c64d59e72166952abbae8cb8fa54d6229
CatonNip <vivinguyen000@gmail.com>
2016-08-23 18:44:24 -0700

    Psudocode the for loop for release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,8 +10,10 @@ horseNames.push("Bob Ross")
 // Create a key value data structure that will assign horseNames as the key and horseColors as the value.
 // Both lists should be the same length 
 // Since this code should work for any number of list items we should find a way to go through each item in the list.
-
-
+// Declare an empty OBJECT
+// FOR LOOP and take the LENGHTH of list horseColors
+	// Declare incrementors
+	// Use BRACKET NOTATION to set the values of horseNames to be the KEY and index of horseColors to be the object VALUE.
 
 
 
