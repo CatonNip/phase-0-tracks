57c3d1294f6e2dad889df065bdaa638870bfbb21
CatonNip <vivinguyen000@gmail.com>
2016-08-24 00:51:09 -0700

    Create algos.js file and psudocode function for release 0

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+//Release 0 //
+// Goals //
+//Write a FUNCTION that takes a LIST of words or phrases and OUTPUTS the longest word or phrase.
+
+//Psudocode//
+//Create a LIST of words or phrases.
+//Declare a FUNCTION that takes a LIST as a parameter.
+	//Set a VARIABLE to an empty string so we can PUSH the data of the longest string into it.
+	//FOR each element in the LIST LOOP through them looking for the longest string LENGTH
+		// CONCENTRATE each STRING into the set VARIABLE
+	//Print the Variable that should contain the longest element in an array
