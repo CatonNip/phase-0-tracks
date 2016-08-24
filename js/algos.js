573e7c23269b46c7274f50fd670d348f678be17d
CatonNip <vivinguyen000@gmail.com>
2016-08-24 00:53:33 -0700

    Write section for driver code test and add new goal to line 4

--- js/algos.js
+++ js/algos.js
@@ -1,6 +1,7 @@
-//Release 0 //
-// Goals //
+//Release 0//
+//Goals//
 //Write a FUNCTION that takes a LIST of words or phrases and OUTPUTS the longest word or phrase.
+//Add driver code to TEST the written function on a few ARRAYS.
 
 //Psudocode//
 //Create a LIST of words or phrases.
@@ -8,4 +9,8 @@
 	//Set a VARIABLE to an empty string so we can PUSH the data of the longest string into it.
 	//FOR each element in the LIST LOOP through them looking for the longest string LENGTH
 		// CONCENTRATE each STRING into the set VARIABLE
-	//Print the Variable that should contain the longest element in an array
+	//Print the variable that should contain the longest element in the list
+
+
+
+	//Driver Code//
