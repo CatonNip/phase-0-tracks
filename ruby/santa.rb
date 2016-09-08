d6b740cf155b530bad932ba992772b424ea6925a
CatonNip <vivinguyen000@gmail.com>
2016-09-08 15:04:02 -0700

    Update age to be a random number between 140

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -6,7 +6,7 @@ class Santa
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+		@age = rand(140)
 	end
 
 	def speak
@@ -31,11 +31,11 @@ class Santa
 end 
 
 #driver code#
-Carl = Santa.new("santa","santa")
-Carl.speak
-Carl.eat_milk_and_cookies("oatmeal")
-Carl.get_mad_at("Vixen")
-p Carl.gender = "female"
+# Carl = Santa.new("santa","santa")
+# Carl.speak
+# Carl.eat_milk_and_cookies("oatmeal")
+# Carl.get_mad_at("Vixen")
+# p Carl.gender = "female"
 
 #repetitive# 
 # santas << Santa.new("agender", "black")
@@ -54,5 +54,4 @@ example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer n
 until santas.length == 10
 	santas << Santa.new(example_genders.sample,example_ethnicities.sample)
 end
-
 p santas
