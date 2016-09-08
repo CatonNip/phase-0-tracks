04c26d24aa646d2064b152daff240cb09a63803e
CatonNip <vivinguyen000@gmail.com>
2016-09-08 13:36:30 -0700

    Test intantces of Santa Class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -34,8 +34,8 @@ santas = []
 #re-factor"
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
 example_genders.length.times do |i|
   santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
-
-# p santas
+p santas
