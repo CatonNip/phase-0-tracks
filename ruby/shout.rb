9282033ec475d5b2100988dbc0c5580bc1e4251e
CatonNip <vivinguyen000@gmail.com>
2016-09-12 07:28:50 -0700

    Write classes inside moduel and driver code to test

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,34 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yell_happily(words)
-  	words + "<3"
+#   def self.yell_happily(words)
+#   	words + "<3"
+# end
+
+# #Driver Code#
+# p Shout.yell_angrily("DARN!")
+# p Shout.yelling_happily("YAY!")
+
+#NEW SHOUT CODE
+
+module Shout
+	def shouting(words)
+		words + "!!!!!!!!! (ꐦ ಠ皿ಠ )"
+	end
 end
 
-#Driver Code#
-p Shout.yell_angrily("DARN!")
-p Shout.yelling_happily("YAY!")
+class Donald_Trump_Supporters
+	include Shout
+end
+
+class Angry_Gamers
+	include Shout
+end
+#driver code
+donald = Donald_Trump_Supporters.new 
+p donald.shouting("Make AMERRRICA GREAT AGAIN")
+
+toxic = Angry_Gamers.new
+p toxic.shouting("UGH THIS TEAM SUCKS")
