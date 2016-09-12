518bc4ad127f9bb2cdbeba94539f07e26b76a373
CatonNip <vivinguyen000@gmail.com>
2016-09-12 07:22:04 -0700

    Add module shout code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,47 +1,6 @@
-###OLD SHOUT CODE###
-# module Shout
-#   def self.yell_angrily(words)
-#     words + "!!!" + " :("
-#   end
-
-#   def self.yelling_happily(words)
-#   	words + "<3" + " ;D"
-#   end
-# end
-
-# #DRIVER CODE
-# p Shout.yell_angrily("DARN!")
-# p Shout.yelling_happily("YAY!")
-#################
-
-#NEW SHOUT CODE
-
 module Shout
-	def shouting(words)
-		words + "!!!!!!!!! (ꐦ ಠ皿ಠ )"
-	end
+  # we'll put some methods here soon, but this code is fine for now!
 end
-
-class Donald_Trump_Supporters
-	include Shout
-end
-
-class Angry_Gamers
-	include Shout
-end
-#driver code
-donald = Donald_Trump_Supporters.new 
-p donald.shouting("Make AMERRRICA GREAT AGAIN")
-
-toxic = Angry_Gamers.new
-p toxic.shouting("UGH THIS TEAM SUCKS")
-
-
-
-
-
-
-
 
 
 
