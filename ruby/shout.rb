a53c326ee19f8278903e147045cfa8470a3e4f2d
CatonNip <vivinguyen000@gmail.com>
2016-09-12 07:26:39 -0700

    Add Driver code to test if moduel works

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -2,5 +2,11 @@ module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
+
+  def self.yell_happily(words)
+  	words + "<3"
 end
 
+#Driver Code#
+p Shout.yell_angrily("DARN!")
+p Shout.yelling_happily("YAY!")
