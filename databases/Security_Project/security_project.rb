2c7eaad699a45659b8610ea007b8034d951580ab
CatonNip <vivinguyen000@gmail.com>
2016-09-28 15:54:51 -0700

    Add goal psudocode to narrow my idea and keep me on track

--- databases/Security_Project/security_project.rb
+++ databases/Security_Project/security_project.rb
@@ -1,19 +1,30 @@
 #PSUDOCODE#
+#Goal for this program is to create something that logs when you logged onto social media 
+#so you can see if anyone else is accessing your account.
+
 #REQUIRE things you will need to set up the database
 #Create the DATABASE TABLE I'll use to track the user's info
 
 require "sqlite3"
 
-db = SQLite3:DATABASE.new("security.db")
+db = SQLite3:DATABASE.new("security_project.db")
 
 user_table_cmd = <<-SQL
 	CREATE TABLE IF NOT EXISTS user(
 		id 	INTEGER PRIMARY KEY,
 		name 	VARCHAR(255),
 		age INT,
+		email VARCHAR(255),
 		location VARCHAR(255),
-		social_media VARCHAR(255)
+		social_media VARCHAR(255),
+		time VARCHAR(255),
+		date VARCHAR(255)
 	)
 SQL
-
+#CREATES a new table
 db.execute(user_table_cmd)
+
+def new_user(dbs, name, age, email, location, social_media, time)
+	db.execute(db)
+end
+	
