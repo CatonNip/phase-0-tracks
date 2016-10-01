4ac2fb1a2483c547cc6e3f6a7617f9f90ca024a5
CatonNip <vivinguyen000@gmail.com>
2016-09-30 20:05:58 -0700

    Add loop to project

--- databases/Security_Project/read_me.txt
+++ databases/Security_Project/read_me.txt
@@ -1,1 +1,5 @@
 You may or may not know it but your devices are not secure. In this world we use so many digital platforms to advertise ourself and share the in and outs of our personal lives online that sometimes we forget that that information is vulnerable to people that would want to use it against us. My goal for this project is to learn why our information is vulnerable and create a program that will allow others to practice safe online procedures that will protect them. Wish me luck! 
+
+Lessons and growth while working on the project:
+-I had a hard time narrowing an idea down.
+-I've been putting so much pressure on myself to think of some awe award winning idea rather than taking a simple approach to things. 
--- databases/Security_Project/security_project.rb
+++ databases/Security_Project/security_project.rb
@@ -30,13 +30,26 @@ def create_user (db, first_name, last_name, age, location, social_media,time,dat
 	db.execute("INSERT INTO users (first_name, last_name, age, location, social_media, time, date) values (?,?,?,?,?,?,?)",[first_name,last_name,age,location,social_media,time,date])
 end
 
-create_user(db, "Jane","Doe",25,"San Francisco","Facebook","Noon","December 25,1999")
-
 print "Security Checker 5000 booting up."
 loop do
 	puts " Please enter your first name."
-	first_name = gets.chomp.to_s
+		first_name = gets.chomp.to_s
 	puts "What's your last name?"
-	last_name = gets.chomp.to_s
+		last_name = gets.chomp.to_s
+	puts "What's your age?"
+		age = gets.chomp.to_i
+	puts "What is your current location?"
+		location = gets.chomp.to_s
+	puts "What social media platform do you want to test for?"
+		social_media = gets.chomp.to_s
+	puts "What is the current time?"
+		time = gets.chomp.to_s
+	puts "What is the current date?"
+		date = gets.chomp.to_s
+
+	create_user(db, first_name, last_name, age, location, social_media,time,date)
+	print "Alright I have stored your data. Would you like to create another user? If not please type 'quit'"
+	continue = gets.chomp.to_s.downcase
+	break if continue = quit
 end
 	
