25d9cdfecec31282411915140136edc8bdbab829
CatonNip <vivinguyen000@gmail.com>
2016-10-03 06:07:13 -0700

    Update Ruby code to include get /contact

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,8 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+#A /contact route that displays an address (you can make up the address).
+get '/contact' do
+  "Dev Boot Campus"
+end
